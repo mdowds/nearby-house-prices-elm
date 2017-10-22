c7a332c02d0e95cee1c4f6a4aeddf2384a267bbf
Matt Dowds <matthewd99@gmail.com>
2017-10-22 15:28:46 +0100

    Added dynamic data fetching for hardcoded outcode

--- main.elm
+++ main.elm
@@ -2,10 +2,19 @@ module NearbyHousePrices exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Http
+import Json.Decode as Decode
+import FormatNumber exposing (format)
+import FormatNumber.Locales exposing (usLocale)
 
 
 main =
-    view model
+    Html.program
+        { init = init "E1"
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
@@ -13,7 +22,8 @@ main =
 
 
 type alias Model =
-    { averagePrice : String
+    { areaName : String
+    , averagePrice : String
     , numberOfTransactions : String
     , detachedAverage : String
     , flatAverage : String
@@ -22,9 +32,31 @@ type alias Model =
     }
 
 
-model : Model
-model =
-    Model "Loading" "Loading" "Loading" "Loading" "Loading" "Loading"
+loadingModel : Model
+loadingModel =
+    Model "Waiting for location..." "Loading" "Loading" "Loading" "Loading" "Loading" "Loading"
+
+
+init outcode =
+    ( loadingModel, getPrices outcode )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ReceivedPrices (Result Http.Error Model)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ReceivedPrices (Ok newModel) ->
+            ( newModel, Cmd.none )
+
+        ReceivedPrices (Err _) ->
+            ( model, Cmd.none )
 
 
 
@@ -34,16 +66,16 @@ model =
 view : Model -> Html msg
 view model =
     div []
-        [ loadingIndicator
+        [ loadingIndicator model
         , mainStatsTable model
         , typeStats model
         ]
 
 
-loadingIndicator : Html msg
-loadingIndicator =
+loadingIndicator : Model -> Html msg
+loadingIndicator model =
     h2 [ id "location" ]
-        [ span [ class "loading" ] [ text "Waiting for location..." ] ]
+        [ text model.areaName ]
 
 
 mainStatsTable : Model -> Html msg
@@ -82,3 +114,78 @@ typeStats model =
             , td [ class "value" ] [ text model.terracedAverage ]
             ]
         ]
+
+
+
+-- HTTP
+
+
+getPrices : String -> Cmd Msg
+getPrices outcode =
+    let
+        url =
+            "https://mdowds.com/nearbyhouseprices/api/prices/outcode/" ++ outcode
+
+        request =
+            Http.get url decodePricesData
+    in
+        Http.send ReceivedPrices request
+
+
+decodePricesData : Decode.Decoder Model
+decodePricesData =
+    Decode.map8
+        parseToModel
+        (Decode.at [ "areaName" ] Decode.string)
+        (Decode.at [ "outcode" ] Decode.string)
+        (Decode.at [ "averagePrice" ] Decode.int)
+        (Decode.at [ "transactionCount" ] Decode.int)
+        (Decode.maybe (Decode.at [ "detachedAverage" ] Decode.int))
+        (Decode.maybe (Decode.at [ "semiDetachedAverage" ] Decode.int))
+        (Decode.maybe (Decode.at [ "flatAverage" ] Decode.int))
+        (Decode.maybe (Decode.at [ "terracedAverage" ] Decode.int))
+
+
+
+-- PARSERS
+
+
+parseToModel : String -> String -> Int -> Int -> Maybe Int -> Maybe Int -> Maybe Int -> Maybe Int -> Model
+parseToModel areaName outcode averagePrice numberOfTransactions detachedAverage semiDetachedAverage flatAverage terracedAverage =
+    Model
+        (parseTitle areaName outcode)
+        (parseToCurrency averagePrice)
+        (toString numberOfTransactions)
+        (parseOptionalInt detachedAverage)
+        (parseOptionalInt semiDetachedAverage)
+        (parseOptionalInt flatAverage)
+        (parseOptionalInt terracedAverage)
+
+
+parseOptionalInt : Maybe Int -> String
+parseOptionalInt optionalInt =
+    case optionalInt of
+        Just int ->
+            parseToCurrency int
+
+        Nothing ->
+            "No data"
+
+
+parseToCurrency : Int -> String
+parseToCurrency int =
+    "£" ++ format { usLocale | decimals = 0 } (toFloat int)
+
+
+parseTitle : String -> String -> String
+parseTitle areaName outcode =
+    areaName ++ " (" ++ outcode ++ ")"
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
